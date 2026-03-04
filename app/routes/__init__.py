add795bf8f1057bfd302babe1487593557de10da
Aravind Ili <iliaravind2@gmail.com>
2026-03-04 20:03:38 +0530

    Wire product routes into shared API router

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 
 from app.routes.orders import router as orders_router
+from app.routes.products import router as products_router
 
 api_router = APIRouter()
 api_router.include_router(orders_router)
+api_router.include_router(products_router)
 
 __all__ = ["api_router"]
